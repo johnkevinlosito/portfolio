bf0b480aaea17fccb6c73fe4347ac2943634468d
John Kevin Losito <48610130+johnkevinlosito@users.noreply.github.com>
2021-10-08 13:31:47 +0800

    fix: responsive navbar

--- components/Navigation.js
+++ components/Navigation.js
@@ -1,23 +1,58 @@
 import React from 'react'
 import Link from "next/link"
+import { useState } from 'react'
 import ThemeSwitch from './ThemeSwitch'
+import { HiMenu, HiX } from 'react-icons/hi'
+import { headline } from '../data/config'
 
 const Navigation = () => {
+  const [active, setActive] = useState(false)
+
+  const menuHandler = () => {
+    setActive(!active)
+  }
   return (
-    <div className="sticky top-0 z-20 py-2 bg-white md:py-6 md:mb-6 dark:bg-black">
-      <div className="container px-4 mx-auto lg:max-w-4xl flex items-center justify-between">
-        <nav>
+    <header className="sticky top-0 z-20 py-2 bg-white md:py-6 md:mb-6 dark:bg-gray-900 shadow-md">
+      <nav className="container px-4 mx-auto lg:max-w-4xl flex flex-col md:items-center md:justify-between md:flex-row-reverse gap-4">
+        <div className="flex flex-row items-center justify-between">
           <Link href="/">
             <a
-              className={"font-medium tracking-wider transition-colors text-gray-900 hover:text-sky-500 uppercase dark:text-white"}
+              className="font-medium tracking-wider transition-colors text-gray-900 hover:text-sky-500 dark:text-white inline-flex md:hidden"
+            >
+              {headline}
+            </a>
+          </Link>
+          <div className="flex justify-between items-center gap-4">
+            <ThemeSwitch />
+            <button className="inline-flex md:hidden cursor-pointer" onClick={menuHandler}>
+              {active ? <HiX size={24} /> : <HiMenu size={24} />}
+
+            </button>
+
+          </div>
+        </div>
+
+
+        <div className={`${active ? 'flex' : 'hidden'
+          } md:flex flex-col flex-grow md:flex-row gap-4 transition-all`}>
+          <Link href="/">
+            <a
+              className="font-medium tracking-wider transition-colors text-gray-900 hover:text-sky-500 uppercase dark:text-white"
             >
               Home
             </a>
           </Link>
-        </nav>
-        <ThemeSwitch />
-      </div>
-    </div>
+          <Link href="/portfolio">
+            <a
+              className="font-medium tracking-wider transition-colors text-gray-900 hover:text-sky-500 uppercase dark:text-white"
+            >
+              Portfolio
+            </a>
+          </Link>
+        </div>
+
+      </nav>
+    </header>
   )
 }
 
