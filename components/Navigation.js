2e2a1f1125bb8181aa2f596cf40d96b4e1f79105
John Kevin Losito <48610130+johnkevinlosito@users.noreply.github.com>
2021-10-10 00:14:36 +0800

    update: spacing and active link style

--- components/Navigation.js
+++ components/Navigation.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import Link from "next/link"
+import { useRouter } from 'next/router';
 import { useState } from 'react'
 import ThemeSwitch from './ThemeSwitch'
 import { HiMenu, HiX } from 'react-icons/hi'
@@ -7,17 +8,17 @@ import { headline } from '../data/config'
 
 const Navigation = () => {
   const [active, setActive] = useState(false)
-
+  const router = useRouter();
   const menuHandler = () => {
     setActive(!active)
   }
   return (
-    <header className="sticky top-0 z-20 py-2 bg-white md:py-6 dark:bg-gray-900 shadow-md">
-      <nav className="container px-4 mx-auto lg:max-w-4xl flex flex-col md:items-center md:justify-between md:flex-row-reverse gap-4">
+    <header className="sticky top-0 z-20 bg-white dark:bg-gray-900 shadow-lg">
+      <nav className="container px-4 mx-auto lg:max-w-4xl flex flex-col md:items-center md:justify-between md:flex-row-reverse md:gap-4">
         <div className="flex flex-row items-center justify-between">
           <Link href="/">
             <a
-              className="font-medium tracking-wider transition-colors text-gray-900 hover:text-sky-500 dark:text-white inline-flex md:hidden"
+              className="font-medium tracking-wider transition-colors text-gray-900 hover:text-sky-500 dark:text-white inline-flex md:hidden py-4"
             >
               {headline}
             </a>
@@ -37,14 +38,14 @@ const Navigation = () => {
           } md:flex flex-col flex-grow md:flex-row gap-4 transition-all`}>
           <Link href="/">
             <a
-              className="font-medium tracking-wider transition-colors text-gray-900 hover:text-sky-500 uppercase dark:text-white"
+              className={`font-medium tracking-wider transition-colors text-gray-900 hover:text-sky-500 uppercase dark:text-white py-2 md:py-6  ${router.pathname == "/" ? "md:border-b-2 md:border-gray-900 dark:border-white" : ""}`}
             >
               Home
             </a>
           </Link>
           <Link href="/portfolio">
             <a
-              className="font-medium tracking-wider transition-colors text-gray-900 hover:text-sky-500 uppercase dark:text-white"
+              className={`font-medium tracking-wider transition-colors text-gray-900 hover:text-sky-500 uppercase dark:text-white py-2 md:py-6  ${router.pathname == "/portfolio" ? "md:border-b-2 md:border-gray-900 dark:border-white" : ""}`}
             >
               Portfolio
             </a>
