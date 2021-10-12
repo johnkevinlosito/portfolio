de8066055307fc7000747970592b59a19b71e5ec
John Kevin Losito <48610130+johnkevinlosito@users.noreply.github.com>
2021-10-12 18:34:21 +0800

    fix: link without noreferrer

--- components/PostContent.js
+++ components/PostContent.js
@@ -10,7 +10,7 @@ const PostContent = ({ article }) => {
         </div>
         <div className="article" dangerouslySetInnerHTML={{ __html: article.html }} />
         <div className="flex justify-center text-md mt-10 mb-32 italic font-bold hover:underline">
-          <a href={article.url} target="_blank">Like this article? React or comment on Dev.to</a>
+          <a href={article.url} target="_blank" rel="noreferrer" >Like this article? React or comment on Dev.to</a>
         </div>
       </div>
     </Section>
