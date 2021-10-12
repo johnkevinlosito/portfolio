595e21e2ec0043a438810d7cb16448a236b18dca
John Kevin Losito <48610130+johnkevinlosito@users.noreply.github.com>
2021-10-12 18:28:44 +0800

    fix: link without noreferrer

--- components/PostContent.js
+++ components/PostContent.js
@@ -6,7 +6,7 @@ const PostContent = ({ article }) => {
     <Section class="-mt-24">
       <div className="px-4 lg:px-0 mt-8 max-w-screen-md mx-auto leading-relaxed">
         <div className="flex justify-center text-md my-4 italic font-bold hover:underline">
-          <a href={article.url} target="_blank">Also posted on Dev.to</a>
+          <a href={article.url} target="_blank" rel="noreferrer" >Also posted on Dev.to</a>
         </div>
         <div className="article" dangerouslySetInnerHTML={{ __html: article.html }} />
         <div className="flex justify-center text-md mt-10 mb-32 italic font-bold hover:underline">
